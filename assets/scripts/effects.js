dbcc8d6dd2f8858bad3e618bf95c09c571a46bbc
henry-lang <henry.langmack@gmail.com>
2023-03-14 10:49:02 -0400

    yo

--- assets/scripts/effects.js
+++ assets/scripts/effects.js
@@ -1,34 +1,37 @@
 'use strict'
 
-function convertRemToPixels(rem) {    
-    return rem * parseFloat(getComputedStyle(document.documentElement).fontSize)
-}
-
+// Initial ascii stuff (Look, I don't like creating a temporary canvas either, but...)
 
 const context = document.createElement('canvas').getContext('2d')
 context.font = getComputedStyle(document.querySelector(".ascii")).font
-console.log(context.font)
 const charDimensions = context.measureText('.') // Monospace font assumed
 const charWidth = charDimensions.width
 const charHeight = charDimensions.fontBoundingBoxAscent
-console.log(charWidth, charHeight)
-// const font = getComputedStyle(document.documentElement).getPropertyValue('--font') + ` ${convertRemToPixels(1.5)}px`
-// const temp = document.createElement('span')
-// temp.innerText = '.'
-// temp.style.font = font
-// document.body.appendChild(temp)
-// document.body.removeChild(temp);
+console.log(`Using font: ${context.font}`)
+console.log(`Found ascii char width: ${charWidth}px, ascii char height: ${charHeight}px`)
 
-const seaWidth = window.innerWidth / charWidth
+const seaElement = document.querySelector('.sea')
+const seaWidth = Math.floor(window.innerWidth / charWidth);
 const seaHeight = Math.floor(window.innerHeight / charHeight * .25)
+const seaTPS = 3;
 const seaText = []
-window.setInterval(() => {
+const seaChars = ['~']
+
+function randomSeaChar() {
+    return seaChars[Math.floor(Math.random() * seaChars.length)]
+}
+
+function updateSea() {
     seaText.length = 0
     for(let i = 0; i < seaHeight; i++) {
         for(let j = 0; j < seaWidth; j++) {
-            seaText.push((Math.random() > (i / seaHeight + 0.1)) ? '~' : ' ')
+            seaText.push((Math.random() > (0.9 * i / seaHeight + 0.1)) ? randomSeaChar() : ' ')
         }
         seaText.push('\n')
     }
-    document.querySelector(".sea").innerText = seaText.join('')
-}, 1000/3)
+
+    seaElement.innerText = seaText.join('')
+}
+
+updateSea();
+window.setInterval(updateSea, 1000 / seaTPS)
