6e398e3cb09b3e9af210fe59067f70c4c1ab8144
henry-lang <henry.langmack@gmail.com>
2023-06-29 22:19:43 -0400

    decimal

--- summer/index.js
+++ summer/index.js
@@ -1,3 +1,29 @@
+function toFixedTrunc(x, n) {
+    const v = (typeof x === 'string' ? x : x.toString()).split('.');
+    if (n <= 0) return v[0];
+    let f = v[1] || '';
+    if (f.length > n) return `${v[0]}.${f.substr(0,n)}`;
+    while (f.length < n) f += '0';
+    return `${v[0]}.${f}`
+}
+
+const decimalDisplay = document.getElementById('decimal-display');
+const minusBtn = document.getElementById('minus-btn');
+const plusBtn = document.getElementById('plus-btn');
+
+let decimalPlaces = 2; // Default value
+
+minusBtn.addEventListener('click', () => {
+    if (decimalPlaces > 0) {
+        decimalPlaces--;
+        decimalDisplay.innerText = decimalPlaces;
+    }
+});
+plusBtn.addEventListener('click', () => {
+    decimalPlaces++;
+    decimalDisplay.innerText = decimalPlaces;
+});
+
 const display = document.getElementById('display');
 
 const startDate = new Date('2023-06-15T12:30:00');
@@ -14,7 +40,7 @@ function getPercentage() {
         return 100;
     } else {
         const percentage = ((currentTimestamp - startTimestamp) / (endTimestamp - startTimestamp)) * 100;
-        return parseFloat(percentage.toFixed(4)); // Round to two decimal places
+        return toFixedTrunc(percentage, decimalPlaces); // Round to two decimal places
     }
 }
 
